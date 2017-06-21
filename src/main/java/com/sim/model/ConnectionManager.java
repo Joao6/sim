c16f062b55c35ba49404e684a6cc3d5e01e60de3
Joao Pedro <joaopedro.dedepaula@gmail.com>
2017-06-21 16:30:26 -0300

    ok

--- src/main/java/com/sim/model/ConnectionManager.java
+++ src/main/java/com/sim/model/ConnectionManager.java
@@ -20,12 +20,12 @@ public class ConnectionManager {
 //Inicio Singleton
     private ConnectionManager() {
         dataSource = new PGPoolingDataSource();
-        dataSource.setDataSourceName("sim");
-        dataSource.setServerName("localhost");
+        dataSource.setDataSourceName("ddqvsiite4n1na");
+        dataSource.setServerName("ec2-23-21-220-152.compute-1.amazonaws.com");
         dataSource.setPortNumber(5432);
-        dataSource.setDatabaseName("sim");
-        dataSource.setUser("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setDatabaseName("ddqvsiite4n1na");
+        dataSource.setUser("leycxmeboxjhtk");
+        dataSource.setPassword("ca2bfa57369e3bfbf5bcdb739db97342c2cac54cc3de6b5da770d7b1d5de1613");
         dataSource.setMaxConnections(30);
         dataSource.setInitialConnections(10);
     }
